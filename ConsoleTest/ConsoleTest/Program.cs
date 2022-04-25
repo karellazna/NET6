fd489b690e6f2d47c2238473bbf6be9c6d85c9d7
Karel Lazna <karel.lazna@infinitesolution.cz>
2022-04-25 16:23:53 +0200

    scitani while

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -1,35 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int konecne_cislo = 0;
 
-string Den_v_tydnu(int cislo_dne)
+do
 {
-
-    switch (cislo_dne)
-    {
-        case 1:
-            return "Pondeli";
-            break;
-        case 2:
-            return "Utery";
-            break;
-        case 3:
-            return "Streda";
-            break;
-        case 4:
-            return "Ctvrtek";
-            break;
-        case 5:
-            return "Patek";
-            break;
-        case 6:
-            return "Sobota";
-            break;
-        case 7:
-            return "Nedele";
-            break;
-            default: return "Chyba";
-        
-    }
+    int cislo = int.Parse(Console.ReadLine());
+    konecne_cislo += cislo;
 }
+while (Console.ReadLine() != "Q");
+ 
+Console.WriteLine(konecne_cislo);
 
-Console.WriteLine(Den_v_tydnu(int.Parse(Console.ReadLine())));
 
+
+
+
+
+
+
+
+
+enum denVTydnu
+{
+    PONDELI,
+    UTERY,
+    STREDA,
+    CTVRTEK,
+    PATEK,
+    SOBOTA,
+    NEDELE
+}
