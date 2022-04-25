98a94b2b2cfcc9880b23391fa415cc64fb49f7e3
Karel Lazna <karel.lazna@infinitesolution.cz>
2022-04-25 11:19:49 +0200

    proměna čísel

---
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 10;
+int b = 20;
+int promena = 0;
+
+promena = a;
+a = b;
+b = promena;
+
+
+
+Console.WriteLine("a:" + a + " b:" + b);
