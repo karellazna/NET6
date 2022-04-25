69496870d39be79b551e61f89d9147698699668d
Karel Lazna <karel.lazna@infinitesolution.cz>
2022-04-25 12:03:55 +0200

    Vetsi menší

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -1,6 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-string var = Console.ReadLine();
-double var_parsed = 1.8 * int.Parse(var) + 32;
-Console.WriteLine(var_parsed);
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+
+int a_parse = int.Parse(a);
+int b_parse = int.Parse(b);
+int c_parse = int.Parse(c);
+
+if (a_parse > b_parse)
+{
+    if(a_parse > c_parse)
+    {
+        Console.WriteLine("nejvetsi je a:" + a_parse);
+    }
+} else if (b_parse > c_parse)
+{
+    Console.WriteLine("Nejvetsi je b:" + b_parse);
+} else
+{
+    Console.WriteLine("Nejvetsi je c:" + c_parse);
+}
