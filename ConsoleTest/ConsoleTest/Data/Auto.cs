89db1aaa1d4203d27ea0477363cd38b1ab986b52
Karel Lazna <karel.lazna@infinitesolution.cz>
2022-04-26 14:04:48 +0200

    auto

---
+++ ConsoleTest/ConsoleTest/Data/Auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.Data
+{
+    internal class Auto
+    {
+        private static string SPZ;
+        private static string Barva;
+        private static double NajeteKM;
+        private static string Znacka;
+
+        public static string GetSPZ()
+        {
+            return SPZ;
+        }
+        public static string GetBarva()
+        {
+            return Barva;
+        }
+        public static double GetNajeteKM()
+        {
+            return NajeteKM;
+        }
+        public static string GetZnacka()
+        {
+            return Znacka;
+        }
+
+    }
+}
