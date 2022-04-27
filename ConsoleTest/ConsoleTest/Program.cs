9f00021b8ef041b6cbd15f58b1ae6f2154318410
Karel Lazna <karel.lazna@infinitesolution.cz>
2022-04-27 11:25:04 +0200

    cisla

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -3,7 +3,7 @@ using ConsoleTest.Data;
 
 int[] cisla = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
 
-var result = cisla.Where(x => 50 > x && x > 50);
+var result = cisla.Where(x => -50 < x && x > 50);
 foreach (int x in result)
 {
     Console.WriteLine(x);
